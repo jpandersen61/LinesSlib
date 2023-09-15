2eb593bab73ad14be0cdc718391481171ddc93aa
Zealand-Jean <55512813+Zealand-Jean@users.noreply.github.com>
2023-09-15 11:19:31 +0200

    Intersection added

--- LinesSlib/LineSegment.cs
+++ LinesSlib/LineSegment.cs
@@ -6,24 +6,43 @@ using System.Threading.Tasks;
 
 namespace LinesSlib
 {
+    /// <summary>
+    /// Line segment class 
+    /// </summary>
     public class LineSegment
     {
+        /// <summary>
+        /// 
+        /// </summary>
         public int Start { get; set; }
         public int End { get; set; }
 
         public override string ToString()
         {
+            //throw new NotImplementedException();
             return $"Start: {Start}, End: {End}";
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="punkt">Punkt der skal checked for..</param>
+        /// <returns></returns>
         public bool Contains(int punkt)
         {
+            
             return (Start <= punkt) && (End >= punkt);
         }
 
         public bool Contains(LineSegment lineSegment)
+        {           
+            return (Start <= lineSegment.Start) && (End >= lineSegment.End);
+        }
+
+        public bool Intersection(LineSegment anotherSegment)
         {
-            throw new NotImplementedException();
+            return    (Start >= anotherSegment.Start) && (End >= anotherSegment.End)
+                   || (Start <= anotherSegment.Start) && (End <= anotherSegment.End);
         }
     }
 }
--- LinesSlibTests/LineSegmentTests.cs
+++ LinesSlibTests/LineSegmentTests.cs
@@ -11,10 +11,12 @@ namespace LinesSlib.Tests
     [TestClass()]
     public class LineSegmentTests
     {
+        const int refStart = 8;
+        const int refEnd = 33;
         LineSegment reference = new LineSegment()
         {
-            Start = 8,
-            End = 33
+            Start = refStart,
+            End = refEnd
         };
 
         [TestMethod()]
@@ -25,20 +27,40 @@ namespace LinesSlib.Tests
         }
 
         [TestMethod()]
+        [DataRow(refStart,refEnd)]
+        [DataRow(refStart+1, refEnd-1)]
+        public void ContainsTestPositive(int start, int end)
+        {
+            LineSegment ls = new LineSegment()
+            {
+                Start = start,
+                End = end
+            };
+            Assert.AreEqual<bool>(true, reference.Contains(ls));
+        }
+
+        [TestMethod()]
         [DataRow(8)]
         [DataRow(9)]
         [DataRow(20)]
         [DataRow(32)]
         [DataRow(33)]
-        public void ContainsTest(int punkt)
+        public void ContainsTestPositive(int point)
         {
-            Assert.AreEqual<bool>(true, reference.Contains(punkt));
+            Assert.AreEqual<bool>(true, reference.Contains(point));
         }
 
         [TestMethod()]
-        public void ContainsTest1()
+        [DataRow(refStart-1, refEnd-1)]
+        [DataRow(refStart + 1, refEnd + 1)]
+        public void IntersectionTestPositive(int start, int end)
         {
-            Assert.Fail();
+            LineSegment ls = new LineSegment()
+            {
+                Start = start,
+                End = end
+            };
+            Assert.AreEqual<bool>(true, reference.Intersection(ls));
         }
     }
 }
